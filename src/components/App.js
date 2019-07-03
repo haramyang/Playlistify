b52855ba5494d92c0aca1d1f4a128b89e11ee163
Haram Yang <haryan@umich.edu>
2019-07-02 22:04:03 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className = "App-header">
+        Your favorite Artists and Tracks
+      </header>
+      <div className = "topArtists">
+        <p> Your Favorite Artists </p>
+      </div>
+      <div className="topArtist">
+        <p> Your Top Artist </p>
+      </div>
+      <div className = "suggestedArtists">
+        <p> place Holder </p>
+      </div>
+      <div className = "footer">
+        <p> footer here </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;

06478376e15a5467c00555538e2d98465140c048
Haram Yang <haryan@umich.edu>
2019-07-03 20:30:32 -0400

    added topArtists component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import TopArtists from './topArtists.js';
 import './App.css';
 
 function App() {
@@ -8,15 +9,18 @@ function App() {
       <header className = "App-header">
         Your favorite Artists and Tracks
       </header>
+
       <div className = "topArtists">
-        <p> Your Favorite Artists </p>
+        <div className = "favArtists">
+          <p> Your Favorite Artists </p>
+        </div>
+        <TopArtists />
       </div>
+
       <div className="topArtist">
         <p> Your Top Artist </p>
       </div>
-      <div className = "suggestedArtists">
-        <p> place Holder </p>
-      </div>
+
       <div className = "footer">
         <p> footer here </p>
       </div>
---
+++ src/components/topArtists.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './topArtists.css';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+class TopArtists extends Component {
+	render() {
+		return (
+			<div className = "TopArtists">
+			  <div className = "left">
+				<Paper>
+				  <div className = "arist">
+				    <Typography variant="headline" component="h3">
+				      Hillsong United
+				    </Typography>
+				    <div className="artistName">
+			          <p>Hillsong United</p>
+			        </div>
+
+		          	<div className = "artistImage">
+		          	  <img src='#' alt='artistImage'/>
+		          	</div>
+
+			        <div className="content">
+			          Information about Hillsong United.
+			        </div>
+
+			        <button className="playArtist">Play Hillsong United</button>
+		          </div>
+			    </Paper>
+			  </div>
+			  <div className = "right">
+			  </div>
+			</div>
+		);
+	}
+}
+
+export default TopArtists

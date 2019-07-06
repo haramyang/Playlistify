5a502f49f24891f26b4665f8af375cc3ff2949d7
Haram Yang <haryan@umich.edu>
2019-07-05 21:26:32 -0400

    created navbar using matieral-ui and flex-boxes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const style = {
+    height: '20vh',
+    background : '#88B9D6',
+};
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className = "App-header">
-          <div className = "header-wrapper">
-            <span className = "logo">Spotifavorify SKRRTTT</span>
-            <span className = "nav-bar">
-              <a href = "/" className = "profile"> Personal Profile </a>
-              <a href = "/top-artists" className = "artist"> Artists </a>
-              <a href = "/top-tracks" className = "tracks"> Tracks </a>
-            </span>
-          </div>
-        </header>
+        <div className = "header">
+          <AppBar style={style}>
+            <Toolbar>
+              <div className = "header-wrapper">
+                <div className = "logo"> 
+                  <div><a href = '/'><i className="large material-icons" style={{fontSize: '50px'}}>library_music</i></a></div>
+                </div>
+                <div className = "nav-bar">
+                  <div><a href = "/" className = "profile"> Personal Profile </a></div>
+                  <div><a href = "/top-artists" className = "artist"> Artists </a></div>
+                  <div><a href = "/top-tracks" className = "tracks"> Tracks </a></div>
+                </div>
+              </div>
+            </Toolbar>
+          </AppBar>
+        </div>
 
         <div className = "content">
           <div className = "content-wrapper">

e5deb2890ef08a7ecd2e892811696c78e2ff6cf6
Haram Yang <haryan@umich.edu>
2019-07-13 10:30:18 -0400

    added componentdidmount to profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 // import profile.css from './profile.css;
 
 class Profile extends Component {
-    constructor() {
-        super();
-        
+    constructor(props) {
+        super(props);
+        this.state = {
+            personalInfo = {},
+        }
+    }
+
+    // The componentDidMount() method runs after the component output has been rendered to the DOM
+    componentDidMount() {
+        fetch(this.props.url, {credentials: 'include'})
+          .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+          })
+          .catch(error => console.log(error));
+          .then((data) => {
+            this.setState({
+               personalInfo = data,
+            });
+          });
+          .catch(error => console.log(error));
     }
 }
 

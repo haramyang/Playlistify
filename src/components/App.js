648755b807162e104634052c60e73c765c77c7e7
Haram Yang <haryan@umich.edu>
2019-07-14 20:37:13 -0400

    moved login to login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Spotify from 'spotify-web-api-js';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import background from '../static/background.jpg'
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Header from "./Header";
+import Login from './login';
+import Profile from "./profile";
+import TopArtists from "./topArtists";
+import TopTracks from "./topTracks";
 
 const spotifyApi = new Spotify();
 
@@ -12,14 +17,14 @@ class App extends Component {
     constructor() {
         super();
         // stores access token and refresh token
-        this.params = this.getHashParams();
-        this.state = {
-            loggedIn: this.params.access_token ? true : false
-        };
-
-        if(this.params.access_token) {
-            spotifyApi.setAccessToken(this.params.access_token);
+        const params = this.getHashParams();
+        const token = params.access_token;
+        if(token) {
+            spotifyApi.setAccessToken(token);
         }
+        this.state = {
+            loggedIn: token ? true : false
+        };
     }
     getHashParams() {
         var hashParams = {};
@@ -30,29 +35,13 @@ class App extends Component {
         }
         return hashParams;
     }
+
     render(){
         return (
             <div className="App">
                 <header id = "App-header">
                     <Header loggedIn = {this.state.loggedIn} />
                 </header>
-
-                <div className = "links">
-                    <div className = "name-wrapper">
-                    {/*
-                    <div className = "App-name">
-                    <p> Spotifavorify </p>
-                    </div>
-                    */}
-                    </div>
-                    <div className = "login-wrapper">
-                        <a href = "http://localhost:8888">
-                            <button type="button" className = "login">
-                                <p> Login </p>
-                            </button>
-                        </a>
-                    </div>
-                </div>
             </div>
         );
     }

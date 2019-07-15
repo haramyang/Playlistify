744e4bf276da0fe93bc167d36527ce98891b6cd0
Haram Yang <haryan@umich.edu>
2019-07-14 20:36:30 -0400

    moved background image only to login page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+//import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './components/App.js';
 import Profile from './components/profile.js';
 import TopArtists from './components/topArtists.js';
 import TopTracks from './components/topTracks.js';
 import Header from './components/Header.js';
+import Login from "./components/login";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 const routing = (
 	<Router>
-		<Route exact path = '/' component = {App} />
+		<Route exact path = '/' component = {Login} />
+		<Route path ='/login' component = {Login} />
 		<Route path = '/profile' component = {Profile} />
 		<Route path = '/top-artists' component = {TopArtists} />
 		<Route path = '/top-tracks' component = {TopTracks} />
 	</Router>
 )
+
 //ReactDOM.render(<Header />, document.getElementById('App-header'));
 ReactDOM.render(routing, document.getElementById('content'));

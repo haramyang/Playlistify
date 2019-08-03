8efff981f6b61c91238fd00348acde472741bbcf
Haram Yang <haryan@umich.edu>
2019-08-03 14:27:57 -0400

    made images accessible for different api calls

--- src/components/js/Item.js
+++ src/components/js/Item.js
@@ -3,6 +3,13 @@ import React, {Component} from 'react';
 let counter = 0;
 class Item extends Component { 
 	render() {
+		let images = [];
+		if(this.props.artist === 1) {
+			images = this.props.item.images;
+		}
+		else{
+			images = this.props.item.album.images;
+		}
 		console.log("ITEM" + counter);
 		console.log(this.props.item.name);
 		console.log(this.props.item);
@@ -10,7 +17,7 @@ class Item extends Component {
 		return(
 			<div className = "item">
 				<a href = {this.props.item.external_urls.spotify}> <p> {this.props.item.name}</p></a>
-				<img src = {this.props.item.album.images[2].url} />
+				<img src = {images[2].url} />
 			</div>
 		)
 	}

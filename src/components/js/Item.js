359ef30e52ec18552ec6445690f44ec91fe46ebd
Haram Yang <haryan@umich.edu>
2019-08-03 14:11:38 -0400

    displaying track name and image

--- src/components/js/Item.js
+++ src/components/js/Item.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 
-const counter = 0;
+let counter = 0;
 class Item extends Component { 
 	render() {
+		console.log("ITEM" + counter);
+		console.log(this.props.item.name);
+		console.log(this.props.item);
+		counter++;
 		return(
 			<div className = "item">
-				<p> {this.props.item.artists.name} </p>
-				<p> {this.props.item.name} </p>
+				<a href = {this.props.item.external_urls.spotify}> <p> {this.props.item.name}</p></a>
+				<img src = {this.props.item.album.images[2].url} />
 			</div>
 		)
 	}

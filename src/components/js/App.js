7ebfabf53e4fb3144dd12b870b4478312b865262
Haram Yang <haryan@umich.edu>
2019-08-10 10:22:25 -0400

    changed route to display

--- src/components/js/App.js
+++ src/components/js/App.js
@@ -58,7 +58,7 @@ class App extends Component {
         } else {
             console.log("LOGGED IN");
             return (
-                <Redirect to = "/top-tracks" />
+                <Redirect to = "/display" />
             );
         }
     }

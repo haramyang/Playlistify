5bb8a932dd07f276f37b7cc7edd97fc7aa828c0c
Haram Yang <haryan@umich.edu>
2019-08-10 10:22:46 -0400

    Added vars to keep track of state

--- src/components/js/Item.js
+++ src/components/js/Item.js
@@ -4,25 +4,34 @@ let counter = 0;
 class Item extends Component { 
 	render() {
 		let images = [];
-		console.log(this.props.type);
+		let name = "";
+		let url = "";
 		switch(this.props.type) {
 			case 'tracks':
 				images = this.props.item.album.images;
+				name = this.props.item.name;
+				url = this.props.item.external_urls.spotify;
 				break;
 			case 'artists':
 				images = this.props.item.images;
+				name = this.props.item.name;
+				url = this.props.item.external_urls.spotify;
 				break;
+			case 'recent':
+				images = this.props.item.track.album.images;
+				name = this.props.item.track.name;
+				url = this.props.item.track.external_urls.spotify;
 			default:
 				break;
 		}
 		console.log("ITEM" + counter);
-		console.log(this.props.item.name);
+		console.log(name);
 		console.log(this.props.item);
 		counter++;
 		return(
 			<div className = "item">
-				<a href = {this.props.item.external_urls.spotify}> <p> {this.props.item.name}</p></a>
-				<img src = {images[1].url} />
+				<a href = {url}> <p>{name} </p></a>
+				<img src = {images[0].url} style = {{height: 100, width: 100}} />
 			</div>
 		)
 	}
